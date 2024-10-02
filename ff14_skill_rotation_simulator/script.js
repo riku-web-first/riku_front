e018ec50fa08fe3bb5d924bbf9a67353f6ae2671
unknown <riku.design.web@gmail.com>
2024-10-02 11:09:21 +0900

    20241002 imgタグのファイル名修正＆js編集

--- ff14_skill_rotation_simulator/script.js
+++ ff14_skill_rotation_simulator/script.js
@@ -1,7 +1,13 @@
 let Skill_Icon = document.querySelectorAll('.skill_icon_list_item')
+let Frame = document.querySelectorAll('.skill')
 function drag() {
-	Skill_Icon[0].addEventListener('dragstart',function(){
-		alert('あああ')
+	Skill_Icon.forEach(function (item, index) {
+		item.addEventListener('dragstart', function () {
+			Frame[0].appendChild(Skill_Icon[0])
+		})
 	})
 }
 drag()
+
+
+
